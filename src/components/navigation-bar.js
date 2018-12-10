9416bfd96dc22748e4266432f9d0ffc9b997951e
Jordan <jordanhaddadi@gmail.com>
2018-12-10 12:19:04 -0500

    created stateless component for nav bar

--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -1,7 +1,17 @@
 import React from 'react';
-
+// import './index.js';
 import './navigation-bar.css';
 
 // The NavigationBar component goes here.  It should be the default export.
+ export default function NavigationBar() {
+     const links = [
+            {text: 'Courses', href: 'http://www.thinkful.com/courses/', id: 1}, 
+            {text: 'Mentorship', href: 'http://www.thinkful.com/mentorship/', id: 2}
+            ];
 
- 
+    const link = links.map(link => (
+        <li key={link.id}><a href={link.href}>{link.text}</a></li>
+    ))
+
+     return(<ul> {link} </ul>);
+ }
